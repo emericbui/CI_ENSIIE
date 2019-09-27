681221f4d245ba39a29504de2378cf6ecea26473
Emeric Bui <emeric.bui@gmail.com>
2019-09-27 11:37:34 +0200

    First commit TP2-union

--- src/interval.test.js
+++ src/interval.test.js
@@ -35,3 +35,32 @@ describe('includes', function () {
         expect(intervalGenerale.includes(interval2)).toBe(false)
     });
 });
+
+describe('union', function () {
+    let intervalGenerale = new Interval(4,8);
+
+    test('Test interval union - return true', () => {
+	let interval2 = new Interval(5,6);
+	let res = new Interval(4,8);
+        expect(intervalGenerale.union(interval2)).toStrictEqual(res)
+    });
+
+    test('Test interval union - return true interval non lie', () => {
+	let interval2 = new Interval(1,4);
+	let res = new Interval(1,8);
+        expect(intervalGenerale.union(interval2)).toStrictEqual(res)
+    });
+
+    test('Test interval union - return true interval non lie', () => {
+	let interval2 = new Interval(1,3);
+	let res = [interval2,intervalGenerale];
+        expect(intervalGenerale.union(interval2)).toStrictEqual(res)
+    });
+
+    test('Test interval union - return true interval non lie', () => {
+	let interval2 = new Interval(10,40);
+	let res = [intervalGenerale,interval2];
+        expect(intervalGenerale.union(interval2)).toStrictEqual(res)
+    });
+
+});
