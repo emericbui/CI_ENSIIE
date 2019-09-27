9890a08030f881522cbb18b004eeb5f11c46f3eb
Emeric Bui <emeric.bui@gmail.com>
2019-09-27 11:25:50 +0200

    first commit TP2-includes

--- src/interval.test.js
+++ src/interval.test.js
@@ -12,3 +12,26 @@ describe('overlaps', function () {
         expect(intervalGenerale.overlaps(interval2)).toBe(false)
     });
 });
+
+
+describe('includes', function () {
+    let intervalGenerale = new Interval(4,8);
+
+    test('Test interval total includes - return true', () => {
+	let interval2 = new Interval(4,8);
+        expect(intervalGenerale.includes(interval2)).toBe(true)
+    });
+
+    test('Test interval  includes - return true', () => {
+	let interval2 = new Interval(5,6);
+        expect(intervalGenerale.includes(interval2)).toBe(true)
+    });
+    test('Test interval total includes - return false', () => {
+	let interval2 = new Interval(1,3);
+        expect(intervalGenerale.includes(interval2)).toBe(false)
+    });
+    test('Test interval includes - return false', () => {
+	let interval2 = new Interval(1,5);
+        expect(intervalGenerale.includes(interval2)).toBe(false)
+    });
+});
